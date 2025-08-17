c03a83cc8018ca73513a3c256984f11d1dd5e4ef
Camila Gomes <camila.gms.santos@email.com>
2025-08-17 14:12:55 -0300

    commit extra.

--- atividade_01/estruturaRepeticao.js
+++ atividade_01/estruturaRepeticao.js
@@ -1,6 +1,7 @@
 console.log("Olá, visitante!!\n");
 console.log("Tópicos dos assuntos estudados em aula:\n")
 
+//Array de tópicos:
 let topicos = ["Lógica de Programação;", "Git e GitHub;", "Linguagem JavaScript;", "Estruturas condicionais;", "Estruturas de Repetição."];
 
 for (let i = 0; i < topicos.length; i++) {
